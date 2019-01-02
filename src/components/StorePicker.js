3a30b44d08e7be02435028b7d0daaf3a92803750
Wayne Collier <ke4tri@gmail.com>
2019-01-02 11:38:10 -0600

    have component printing via the index.js file

---
+++ src/components/StorePicker.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <form className="store-selector">
+          <h2>Please Enter A Store</h2>
+        </form>
+      </React.Fragment>
+    )
+  }
+}
+
+export default StorePicker;
