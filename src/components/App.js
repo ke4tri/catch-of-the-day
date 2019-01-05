25272bc248e201bd3c82757de2f6176fe63e579d
Wayne Collier <ke4tri@gmail.com>
2019-01-04 23:08:45 -0600

    orders printing

--- src/components/App.js
+++ src/components/App.js
@@ -26,6 +26,13 @@ loadSampleFishes = () => {
   this.setState({ fishes: sampleFishes });
 };
 
+addToOrder = (key) => {
+  const order = { ...this.state.order };
+  order[key] = order[key] + 1 || 1 ;
+  this.setState({ order });
+
+};
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -33,10 +40,17 @@ loadSampleFishes = () => {
           <Header tagline="Fresh Seafood Market" />
             <ul className="fishes">
               {Object.keys(this.state.fishes).map(key => 
-              <Fish key={key} details={this.state.fishes[key]} />)}
+              <Fish 
+                key={key}
+                index={key}
+                details={this.state.fishes[key]} 
+                addToOrder={this.addToOrder} 
+              />
+              )}
             </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order}/>
+        {/* <Order {...this.state} />         */}
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,8 +2,13 @@ import React from 'react';
 import {formatPrice } from '../helpers';
 
 class Fish extends React.Component {
+  // handleClick = () => {
+  //   this.props.addToOrder(this.props.index);
+  // }
   render() {
     const {image, name, price, desc, status } = this.props.details;
+    const isAvailable = status === 'available';
+    // const isTrue = status === true;
 
     return <li className="menu-fish">
       <img src={image} alt={name} />
@@ -12,6 +17,9 @@ class Fish extends React.Component {
         <span className="price">{formatPrice(price)}</span>
       </h3>
       <p>{desc}</p>
+      <button disabled={!isAvailable} onClick={() => this.props.addToOrder(this.props.index)}>
+      {isAvailable ? 'Add To Cart' : 'Sold out!'}</button>
+      {/* <button disabled={!isTrue}>Add To Cart</button> */}
     </li>;
   }
 }
