f278873aeb6ebb5f0538ef0f1fd1e41c7b90f646
Wayne Collier <ke4tri@gmail.com>
2019-01-05 15:26:19 -0600

    test pr

--- src/helpers/apiKeys.js
+++ src/helpers/apiKeys.js
@@ -1,10 +1,6 @@
 import firebase from "firebase";
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBj-OsiXavCndqqoKsrQYxHBQeJhKvA0Xo",
-  authDomain: "catch-of-the-day-2-65302.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-2-65302.firebaseio.com"
-});
+
 
 export default firebaseApp ;
 
@@ -14,4 +10,5 @@ export default firebaseApp ;
     //   apiKey: "AIzaSyCM87dhFikNPjKKpgXP3e_lMJpqoNWhVpo",
     //   authDomain: "catch-of-the-day-94e8a.firebaseapp.com",
     //   databaseURL: "https://catch-of-the-day-94e8a.firebaseio.com"
+    // another comment
     // });
