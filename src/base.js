924f2d4b56403df44aca3ea5b8b711ddfc5847bb
Wayne Collier <ke4tri@gmail.com>
2019-01-05 15:05:25 -0600

    correction to the apiKeys

--- src/base.js
+++ src/base.js
@@ -1,11 +1,5 @@
 import Rebase from 're-base';
-import firebase from "firebase";
-
-const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCM87dhFikNPjKKpgXP3e_lMJpqoNWhVpo",
-    authDomain: "catch-of-the-day-94e8a.firebaseapp.com",
-    databaseURL: "https://catch-of-the-day-94e8a.firebaseio.com"
-});
+import firebaseApp from '../src/helpers/apiKeys';
 
 const base = Rebase.createClass(firebaseApp.database());
 
---
+++ src/helpers/apiKeys.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyBj-OsiXavCndqqoKsrQYxHBQeJhKvA0Xo",
+  authDomain: "catch-of-the-day-2-65302.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-2-65302.firebaseio.com"
+});
+
+export default firebaseApp ;
+
+
+
+    // const firebaseApp = firebase.initializeApp({
+    //   apiKey: "AIzaSyCM87dhFikNPjKKpgXP3e_lMJpqoNWhVpo",
+    //   authDomain: "catch-of-the-day-94e8a.firebaseapp.com",
+    //   databaseURL: "https://catch-of-the-day-94e8a.firebaseio.com"
+    // });
